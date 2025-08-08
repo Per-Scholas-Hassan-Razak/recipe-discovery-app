740de05531c02d575827612b7c50677fc44e1af9
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-08-08 11:45:15 -0400

    fetching categories from meal api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  
 
-  return (
-    <>
+  const [categories,setCategories] = useState([])
 
-    </>
-  )
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/categories.php"
+      );
+      if (!response.ok) {
+        throw new Error("API End point ");
+      }
+      const data = await response.json();
+      setCategories(data.categories)
+    } catch (e) {
+      if (e instanceof Error) {
+        console.error(e.message);
+      } else {
+        console.error(e);
+      }
+    } finally {
+      console.log("in the finally");
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  return <></>;
 }
 
-export default App
+export default App;
